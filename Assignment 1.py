32d970f60e33a5ac56c715986c4412b5c0321413
Dumacrevano <dumacrevano.2000@gmail.com>
2017-09-25 15:28:37 +0700

    TRY

--- Assignment 1.py
+++ Assignment 1.py
@@ -3,7 +3,7 @@ time_on_road = int(input("Time:\n"))
 acceleration = int(input("Acceleration:\n"))
 Dist = int(input("Distance:\n"))
 Speed_lim = 60
-v = velocity*time_on_road + acceleration * time_on_road   # speed at the end of journey formula
+v = velocity + acceleration * time_on_road   # speed at the end of journey formula
 s = velocity*time_on_road + (acceleration / 2 * time_on_road ** 2)  # reached distance formula
 x = 0
 print("one * represent 10M:")
@@ -13,9 +13,9 @@ while x < time_on_road:
     y = int(s / 10) * "*"
     print("duration" + str(x) + "=" + y)
 if v >= 60:
-    print("speed limit exceeded,(the speed is " + str(v) + "M/S")
+    print("speed limit exceeded,(the speed is " + str(v) + "M/S)")
 else:
-    print("speed under the limit"+"(,the speed is " + str(v) + "M/S")
+    print("speed under the limit"+"(,the speed is " + str(v) + "M/S)")
 if s >= Dist:
     print("you have reached your destination"+" (destination reached"+ str(s)+"M)")
 else:
